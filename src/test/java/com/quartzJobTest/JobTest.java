9ac10efa369d8c999cdbf0730d51acc274df39ae
fengyazhou <1280096673@qq.com>
2019-03-01 16:29:43 +0800

    测试test分支

--- src/test/java/com/quartzJobTest/JobTest.java
+++ src/test/java/com/quartzJobTest/JobTest.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+//test分支
 public class JobTest {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Test
